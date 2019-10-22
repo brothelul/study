6e309333373355ce37fd656ff4de94f4df1930f0
chenglu <chenglu@deepexi.com>
2019-10-22 14:14:53 +0800

    栈问题求解

---
+++ algorithm/src/main/java/com/ddup/leetcode/stack/q1047/Main.java
@@ -0,0 +1,18 @@
+package com.ddup.leetcode.stack.q1047;
+
+/**
+ * <p> 删除重复项 </p>
+ * 例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+ *
+ * @author chenglu
+ * @date 2019/10/22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("abbaca"));
+    }
+
+    public static String removeDuplicates(String S) {
+        return S;
+    }
+}
