34770793b049724de8d0c73ce5b73d5a5c2a6471
chenglu <chenglu@deepexi.com>
2019-11-21 18:22:00 +0800

    Spring 学习

--- springstudy/src/main/java/com/ddup/springstudy/Application.java
+++ springstudy/src/main/java/com/ddup/springstudy/Application.java
@@ -3,6 +3,8 @@ package com.ddup.springstudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * <p> </p>
--- springstudy/src/main/java/com/ddup/springstudy/eventtest/EventTest.java
+++ springstudy/src/main/java/com/ddup/springstudy/eventtest/EventTest.java
@@ -1,8 +1,10 @@
 package com.ddup.springstudy.eventtest;
 
 import com.ddup.springstudy.domain.EventDTO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -11,11 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
  * @author chenglu
  * @date 2019/11/8
  */
-@Component
+@Service
 public class EventTest {
     @EventListener
     @Transactional
     public void listenEvent(EventDTO e) {
         System.out.println(Thread.currentThread().getName()+""+e);
+    }
+
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.scan("com.ddup");
+        applicationContext.refresh();
+        System.out.println(applicationContext.getBean("eventTest"));
     }
 }
