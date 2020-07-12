822fad76d27697cffeeec24ff6e69e2da156c561
chenglu <1285823170@qq.com>
2020-07-12 23:42:33 +0800

    求1的个数

---
+++ algorithm/src/main/java/com/ddup/leetcode/offer/Q42.java
@@ -0,0 +1,41 @@
+package com.ddup.leetcode.offer;
+
+/**
+ * <p>
+ *     输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
+ *
+ * 例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 12
+ * 输出：5
+ * 示例 2：
+ *
+ * 输入：n = 13
+ * 输出：6
+ *  
+ *
+ * 限制：
+ *
+ * 1 <= n < 2^31
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * </p>
+ *
+ * @author chenglu
+ * @date 2020/7/12 0012 23:16
+ */
+public class Q42 {
+    public static void main(String[] args) {
+        System.out.println(countDigitOne(123));
+    }
+
+    public static int countDigitOne(int n) {
+
+    }
+}
