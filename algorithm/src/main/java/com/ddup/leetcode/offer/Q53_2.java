c2548b2b0f1e40525696c65d6649d37fd49abca7
chenglu <chenglu@deepexi.com>
2020-07-25 16:25:31 +0800

    数组缺失数

---
+++ algorithm/src/main/java/com/ddup/leetcode/offer/Q53_2.java
@@ -0,0 +1,45 @@
+package com.ddup.leetcode.offer;
+
+/**
+ * <p>
+ *     一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
+ *     在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: [0,1,3]
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: [0,1,2,3,4,5,6,7,9]
+ * 输出: 8
+ *  
+ *
+ * 限制：
+ *
+ * 1 <= 数组长度 <= 10000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * </p>
+ *
+ * @author chenglu
+ * @date 2020/7/25
+ */
+public class Q53_2 {
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[] {0,1,2,3,4,5,6,7,9}));
+    }
+
+    public static int missingNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
